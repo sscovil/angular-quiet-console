5621f4376cdaeaa7883f9b2fa17067e16d0bc8ad
Shaun Scovil <sscovil@gmail.com>
2015-09-01 18:13:49 -0400

    Create quiet-console.js

---
+++ quiet-console.js
@@ -0,0 +1,43 @@
+(function () {
+    'use strict';
+    
+    angular.module('quiet-console', [])
+        .provider('$quietConsole', QuietConsole)
+    ;
+    
+    function QuietConsole() {
+        var _ignorePatterns, provider;
+
+        _ignorePatterns = [];
+
+        provider = this;
+        provider.ignore = ignore;
+        provider.$get = Factory;
+
+        if (console && console.warn) {
+            console._warn = console.warn;
+            console.warn = warn;
+        }
+
+        function Factory() {
+            var factory;
+
+            factory = this;
+            factory.ignore = ignore;
+        }
+
+        function ignore(stringOrPattern) {
+            _ignorePatterns.push(stringOrPattern);
+        }
+
+        function warn(message) {
+            if (_ignorePatterns.length)
+                for (var i = 0; i < _ignorePatterns.length; i++)
+                    if (message.match(_ignorePatterns[i]) !== null)
+                        return;
+
+            console._warn.apply(console, arguments);
+        }
+    }
+
+})();
